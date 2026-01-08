00c75b29f24e34d2e469f49049dd98fd715eab43
Palwinder Singh <Palwinder.Singh@quest.com>
2026-01-08 05:47:13 -0800

    TFS #650905: [BUG] [VKB) - Secrets Broker: Certificate Upload is Broken Added check for both encoding pcks12 and PEM encoding

--- SafeguardDevOpsService/Extensions/CertificateExtensions.cs
+++ SafeguardDevOpsService/Extensions/CertificateExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace OneIdentity.DevOps.Extensions
@@ -30,8 +31,21 @@ namespace OneIdentity.DevOps.Extensions
                 // We convert the bytes to a string to use the PEM parser.
                 string pemString = Encoding.UTF8.GetString(rawData);
 
-                // This works if the PEM contains both the CERTIFICATE and PRIVATE KEY
-                return X509Certificate2.CreateFromPem(pemString);
+                // 1. Load the public certificate
+                var cert = X509CertificateLoader.LoadCertificate(rawData);
+
+                // 2. Load the private key if it exists in the string
+                if (pemString.Contains("PRIVATE KEY"))
+                {
+                    using var rsa = RSA.Create();
+                    // ImportFromPem automatically handles both 'Universal 2' and 'Universal 16'
+                    rsa.ImportFromPem(pemString);
+
+                    // 3. Link them together
+                    return cert.CopyWithPrivateKey(rsa);
+                }
+
+                return cert;
             }
         }
     }
